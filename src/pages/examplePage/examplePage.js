62afb58a897ee5710868d0d7eaa5e18769a57232
Mishulka <misailgromov@gmail.com>
2025-02-14 22:29:07 +0300

    add event listener function

--- src/pages/examplePage/examplePage.js
+++ src/pages/examplePage/examplePage.js
@@ -2,14 +2,9 @@ const template = `
 <div>
   <h2>Example Page Content 2</h2>
   <p>This is content from the Example Page template.</p>
-  <p>Data:</p>
-  <ul>
-    {{#each users}}
-      {{> userPartial userName=userName}}
-    {{/each}}
-  </ul>
-  <p>Current Page</p>
+  <p>Components:</p>
+  {{> Button}}
 </div>
 `;
 
-export { template as ExamplePage };
+export { template as AllComponentsPage };
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
     }
   },
   css: {
+    modules: {
+ 
+    },
     postcss: {
       plugins: [
         require('autoprefixer')
