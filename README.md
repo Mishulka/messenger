42d46900413c64661b00b3a1b945e911e57a9483
Mishulka <misailgromov@gmail.com>
2025-02-16 18:09:31 +0300

    made spa transitions between pages

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Это **веб**-приложение с чатом построенное на ванильном JS с использованием шаблонизатора Handlebars
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/c909ca8a-db4c-4137-a796-81a21b20bb86/deploy-status)](https://app.netlify.com/sites/mishagro-messenger/deploys)
-
+##Ссылка на задеплоенный сайт:
+https://mishagro-messenger.netlify.app/
 
 ##Запуск на локальном компьютере
 Установка зависимостей без которых приложение работать не будет:
@@ -13,4 +14,3 @@ npm run start
 Если нужен запуск только фронтенд части для разработки:
 npm run dev
 
-##Ссылка на задеплоенный сайт:
--- src/app.js
+++ src/app.js
@@ -1,78 +1,107 @@
-//libs
+
 import Handlebars from "handlebars";
 import * as Pages from "./pages";
 
-//import components
+
 import Dropdown from './partials/dropdown/Dropdown';
 import Field from './partials/field/Field';
 import Input from './partials/input/Input';
 import SearchInput from './partials/searchInput/SearchInput';
 import SettingField from './partials/settingField/SettingField';
 import button from './partials/button/button';
+import Link from './partials/link/Link';
 
 
-//register components
+
 Handlebars.registerPartial('Button', button);
 Handlebars.registerPartial('Dropdown', Dropdown);
 Handlebars.registerPartial('Field', Field);
 Handlebars.registerPartial('Input', Input);
 Handlebars.registerPartial('SearchInput', SearchInput);
 Handlebars.registerPartial('SettingField', SettingField);
-
+Handlebars.registerPartial('Link', Link);
+const pages = {
+    'Login': [ Pages.Login, {} ], 
+    '505': [Pages.ServerError, {}], 
+    'NewAvatar': [Pages.NewAvatar, {}], 
+    'Profile': [Pages.Profile, {}], 
+    'Signin': [Pages.Signin, {}], 
+    'AllBlocks': [Pages.AllComponentsPage, {}], 
+    'NotFound': [Pages.NotFound, {}] 
+};
 
 export default class App {
-    
     constructor() {
         this.state = {
-            currentPage: 'AllBlocks'
+            currentPage: 'Login'
         }
         this.appElement = document.getElementById('app');
     };
     
     render() {
         switch (this.state.currentPage) {
-            case "AllBlocks":
-                this.renderPage(Pages.AllComponentsPage);
+            case "Login":
+                this.renderPage('Login');
                 break;
             case "505":
-                this.renderPage(Pages.ServerError);
-                break;
-            case "Login":
-                this.renderPage(Pages.Login);
+                this.renderPage('505'); 
                 break;
             case "NewAvatar":
-                this.renderPage(Pages.NewAvatar);
+                this.renderPage('NewAvatar'); 
                 break;
             case "Profile":
-                this.renderPage(Pages.Profile);
+                this.renderPage('Profile'); 
                 break;
             case "Signin":
-                this.renderPage(Pages.Signin);
+                this.renderPage('Signin'); 
+                break;
+            case "AllBlocks":
+                this.renderPage('AllBlocks'); 
                 break;
             default:
-                this.renderPage(Pages.NotFound);
+                this.renderPage('NotFound'); 
         }
         this.attachEventListeners();
     }
 
     renderPage(page) {
-        const template = Handlebars.compile(page);
-        this.appElement.innerHTML = template();
+        const [ source, context ] = pages[page];
+        const container = document.getElementById('app');
+
+        const temlpatingFunction = Handlebars.compile(source);
+        console.log('html', temlpatingFunction(context))
+        container.innerHTML = temlpatingFunction(context);
+}
+    
+
+    attachEventListeners() {     
+        const allButtons = document.querySelectorAll('button');
+        const allLinks = document.querySelectorAll('a');
+        allButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const pageName = link.dataset.page;
+                if (pageName) {
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                    this.state.currentPage = pageName; 
+                    this.render(); 
+                }
+            });
+        });
+        allLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                const pageName = link.dataset.page;
+                if (pageName) {
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                    this.state.currentPage = pageName; 
+                    this.render(); 
+                }
+            });
+        });
     }
 
-    attachEventListeners() {
-        //example event:
-        // const PressedButton = document.getElementById('btn-test');
-        // if(PressedButton) {
-        //     PressedButton.addEventListener('click', () => {
-        //         console.log("log about press this button")
-        //         this.setState({ currentPage: 'PageAfterPressingThisButton' });
-        //     })
-        // }
-
-    }
-
-    setState(newState) {
+    setPage(newState) {
         this.state = { ...this.state, ...newState };
         this.render();
     }
