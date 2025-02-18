dec1621d8aa9a1b77497dbc585f7a683585264c5
Mishulka <misailgromov@gmail.com>
2025-02-18 17:59:01 +0300

    make layout profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,6 +1,21 @@
+import styles from './style.module.pcss';
+
 const template = `
-{{> Link text="Вернуться" data-page="SelectChat"}}
-<h1>Your Profile</h1>
+<div class="${styles.profile_container}">
+    <h1>Your Profile</h1>
+    <img src="{{user.userAvatar}}" alt="Фото профиля"/>
+    {{> SettingField label-name="Почта" placeholder="ivanivanov@mail.com"}}
+    {{> SettingField label-name="Логин" placeholder="ivan"}}
+    {{> SettingField label-name="Имя" placeholder="Иван"}}
+    {{> SettingField label-name="Фамилия" placeholder="Иванов"}}
+    {{> SettingField label-name="Имя в чате" placeholder="Ванек"}}
+    {{> SettingField label-name="Телефон" placeholder="+7(900)900-90-90"}}
+<div class="${styles.profile_container}">
+    {{> SettingField label-name="Изменить данные" change=true}}
+    {{> SettingField label-name="Изменить Пароль" change=true}}
+    {{> Link text="Выйти" data-page="SelectChat" exit=true}}
+</div>
+</div>
 `
 
 export { template as Profile };
