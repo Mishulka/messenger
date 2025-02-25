0aeeda10eb2e2cd23e7171c3e51fa416232d7db0
Mishulka <misailgromov@gmail.com>
2025-02-25 13:50:38 +0300

    fixed form semantic in editprofle and select chat

--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -4,15 +4,18 @@ const template = `
 <div class="${styles.profile_container}">
     <h1>Edit Profile</h1>
     <img src="{{user.userAvatar}}" alt="Фото профиля"/>
-    {{> SettingField label-name="Почта" placeholder="ivanivanov@mail.com" name="email"}}
-    {{> SettingField label-name="Логин" placeholder="ivan" name="first_name"}}
-    {{> SettingField label-name="Имя" placeholder="Иван" name="login"}}
-    {{> SettingField label-name="Фамилия" placeholder="Иванов" name="second_name"}}
-    {{> SettingField label-name="Имя в чате" placeholder="Ванек" name="display_name"}}
-    {{> SettingField label-name="Телефон" placeholder="+7(900)900-90-90" name="phone"}}
-<div class="${styles.profile_container}">
-    {{> Button text="Сохранить" data-page="Profile"}}
-</div>
+    <form>
+        {{> SettingField label-name="Почта" placeholder="ivanivanov@mail.com" name="email"}}
+        {{> SettingField label-name="Логин" placeholder="ivan" name="first_name"}}
+        {{> SettingField label-name="Имя" placeholder="Иван" name="login"}}
+        {{> SettingField label-name="Фамилия" placeholder="Иванов" name="second_name"}}
+        {{> SettingField label-name="Имя в чате" placeholder="Ванек" name="display_name"}}
+        {{> SettingField label-name="Телефон" placeholder="+7(900)900-90-90" name="phone"}}
+        <div class="${styles.profile_container}">
+            {{> Button text="Сохранить" data-page="Profile" type="submit"}}
+        </div>
+    </form>
+    
 </div>
 `
 
