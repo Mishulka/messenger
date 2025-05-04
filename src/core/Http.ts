976da0dcc9d98f58a148fbbba2575c7d5db24bf5
Mishulka <misailgromov@gmail.com>
2025-05-04 13:51:11 +0300

    -feat: made Http class for requests

---
+++ src/core/Http.ts
@@ -0,0 +1,57 @@
+enum METHOD {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    PATCH = 'PATCH'
+};
+
+
+type Options = {
+    method: METHOD;
+    data?: string;
+};
+
+type OptionsWithoutMethod = Omit<Options, 'method'>;
+
+class Http  {  
+    get(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+        return this.request(url, {...options, method: METHOD.GET});
+    };
+     
+    request<XMLHttpRequest>(url: string, options: Options = { method: METHOD.GET }): Promise<XMLHttpRequest> {
+    
+        const {method, data} = options;
+    
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+        
+            xhr.open(method, url);
+            xhr.setRequestHeader('Content-Type', 'text/plain', );
+            //key for test api. Will delete it later
+            xhr.setRequestHeader('x-api-key', 'reqres-free-v1', );
+    
+            xhr.onload = function() {
+                resolve(xhr);
+            }
+    
+            const handleError = (err: unknown) => {
+                console.log(err);
+            }
+    
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+    
+            if (method === METHOD.GET || !data) {
+                xhr.send();
+            }else {
+                xhr.send((data));
+            }
+        })     
+    }
+}
+
+
+
+export { Http, METHOD };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import App from './app.js'
 import './main_styles/style.pcss';
+import { Http, METHOD } from './core/Http.js';
 
+new Http().get('https://reqres.in/api/users?page=2')
+    .then((xhr) => {
+        console.log('Res:', xhr.responseText);
+    })
 
 
 
